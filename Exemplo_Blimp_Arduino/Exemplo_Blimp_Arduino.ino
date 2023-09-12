47563613f6eb789d9cfd562ff3a21f57420b62a4
d4nkali <133923419+d4nkali@users.noreply.github.com>
2023-09-12 20:48:50 -0300

    Exemplo_piscar_led

---
+++ Exemplo_Blimp_Arduino/Exemplo_Blimp_Arduino.ino
@@ -0,0 +1,15 @@
+// Exemplo de ligar e desligar led arduino.
+
+void setup {
+
+    pinMode(13, OUTPUT);
+
+}
+
+void loop {
+
+    digitalWhite(13, HIGH);
+    delay(1000);
+    digitalWhite(13, LOW);
+
+}
